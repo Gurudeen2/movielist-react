cbafa9c61267f3f93e4ca458df4537b20c218e36
ControlPlus <akeemtolanifatai@gmail.com>
2023-02-28 13:26:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // const fetchMoviesHandler = useCallback(async () => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://movito10-default-rtdb.firebaseio.com/movies.json'
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error('Something went wrong!');
+  //     }
+
+  //     const data = await response.json();
+  //     const loadedMovies = [];
+
+  //     for (const key in data) {
+  //       loadedMovies.push({
+  //         id: key,
+  //         title: data[key].title,
+  //         openingText: data[key].openingText,
+  //         releaseDate: data[key].releaseDate,
+  //       });
+  //     }
+
+  //     setMovies(loadedMovies);
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  //   setIsLoading(false);
+  // }, []);
+
+  const fetchMoviesHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json()
+   
+      const transformData = data.results.map((moviesData) => {
+        return {
+          id: moviesData.episode_id,
+          title: moviesData.title,
+          openingText: moviesData.opening_crawl,
+          releaseDate: moviesData.release_date,
+        };
+      });
+      setMovies(transformData);
+   
+  };
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      "https://movito10-default-rtdb.firebaseio.com/movies.json",
+
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Context-Type": "application/json",
+        },
+      }
+    );
+
+    fetchMoviesHandler();
+  };
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
